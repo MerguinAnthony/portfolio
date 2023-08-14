8b5d841b7f9b52def0e03c11a4940bc9540590b2
Anthony Merguin <anthonymerguin@MacBook-Air-de-Anthony.local>
2023-08-14 11:43:33 +0200

    📝 Minor changes

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/styles/bootstrap.min.css">
     <link rel="stylesheet" href="assets/styles/styles.css">
-    <link rel="icon" href="assets/img/amlsf.png" type="image/png"> 
+    <link rel="icon" href="assets/img/aml.png" type="image/png"> 
     <title><?= $admin_info['fullname'] ?> | <?=$admin_info['job'] ?></title>
   </head>
   <body>
     <div id="table">
       <div id="centeralign">
-        <img id="logo" src="assets/img/aml.jpeg" alt="logo">
         <h1><?= $admin_info['fullname'] ?> | <?=$admin_info['job'] ?></h1>
         <p id="description"><?=$admin_info['description']?></p>
         <p class="mt-5"><?=$admin_info['dev']?></p>
